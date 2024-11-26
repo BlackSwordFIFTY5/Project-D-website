06246b321f93c09058e93be6059ded55bc5ec078
BlackSwordFIFTY5 <135648102+BlackSwordFIFTY5@users.noreply.github.com>
2024-11-26 20:41:10 +0500

    Added more JS for new elements

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ function isMobileDevice() {
 window.addEventListener('load', function() {
     // Hide the loading screen when everything has loaded
     document.getElementById('loading').style.display = 'none';
+
+    const filePath = window.location.pathname;
+
+    const fileName = filePath.split('/').pop(); // Extract the last part of the path
+
+    if (fileName === 'provinces.html') {
+        let province = this.sessionStorage.getItem('province');
+        togglePrefecture(province);
+    }
+    
+        if (fileName === 'cars.html') {
+        let car = this.sessionStorage.getItem('car');
+        toggleCar(car)
+    }
 });
 
 
@@ -83,6 +97,8 @@ function toggleProvinceData(targetProvince) {
     }
 }
 
+
+if (document.getElementById('gunma'))
 $("#gunma-map").on('click', function(e) {
     e.preventDefault(); // Prevent default link behavior
     toggleProvinceData('gunma');
@@ -119,19 +135,19 @@ $("#kanagawa-map").on('click', function(e) {
 });
 
 function removeTracks () {
-    const provinces = ['gunma', 'tochigi', 'ibaraki', 'saitama', 'chiba', 'tokyo', 'kanagawa'];
+    const prefectures = ['gunma', 'tochigi', 'ibaraki', 'saitama', 'chiba', 'tokyo', 'kanagawa'];
 
-    provinces.forEach(function (province) {
-        if (document.getElementById(`${province}-area-active`)) {
-            document.getElementById(`${province}-area-active`).id = `${province}-area`;
-            document.getElementById(`${province}-province-data`).style.display = 'none';
-            console.log(`Removing ${province}`)
+    prefectures.forEach(function (prefecture) {
+        if (document.getElementById(`${prefecture}-area-active`)) {
+            document.getElementById(`${prefecture}-area-active`).id = `${prefecture}-area`;
+            document.getElementById(`${prefecture}-province-data`).style.display = 'none';
+            console.log(`Removing ${prefecture}`)
         }
     })
 }
-
-const backButtonP = document.querySelectorAll('#back-button-p');
     
+const backButtonP = document.querySelectorAll('#back-button-p');
+
 backButtonP.forEach(function (button) {
     button.addEventListener('click', function () {
     
@@ -237,8 +253,383 @@ if(document.getElementById("main-menu")) {
 
 }
 
-document.getElementById("barrier").addEventListener('click', function() {
-    // Get the current number from the textContent, parse it as an integer, increment by 1, and then set the new value
-    var currentNumber = parseInt(document.getElementById("blogs-number").textContent);
-    document.getElementById("blogs-number").textContent = currentNumber + 1;
+
+if (document.getElementById("barrier")) {
+    document.getElementById("barrier").addEventListener('click', function() {
+        // Get the current number from the textContent, parse it as an integer, increment by 1, and then set the new value
+        var currentNumber = parseInt(document.getElementById("blogs-number").textContent);
+        document.getElementById("blogs-number").textContent = currentNumber + 1;
+    });
+}
+
+function removeSections() {
+
+    const sections = ['gunma', 'tochigi', 'saitama', 'ibaraki', 'kanagawa', 'chiba', 'tokyo'];
+    const provinces = ['akina', 'akagi', 'myogi', 'usui', 'momiji', 'irohazaka', 'enna-skyline', 'happogahara', 'shomaru', 'sadamine', 'maze', 'tsuchisaka', 'tsukuba', 'yabitsu', 'nagao', 'nanamagari','tsubaki'];
+
+    sections.forEach(function (section) {
+        document.getElementById(`${section}-section`).style.display = 'none';
+    })
+    provinces.forEach(function (province) {
+        document.getElementById(`${province}-section`).style.display = 'none';
+        if(document.getElementById(`${province}-section-button`)) {
+            document.getElementById(`${province}-section-button`).className = 'sidenav-button';
+        }
+    })
+}
+
+
+function toggleTracks(prefecture) {
+
+    const prefectureTracks = document.getElementById(`${prefecture}-tracks`);
+    const prefectureButton = document.getElementById(`${prefecture}-section-button`);
+    const prefectureButtonActive = document.getElementById(`${prefecture}-section-button-active`);
+
+    if(!prefectureButtonActive) {
+        prefectureButton.id = `${prefecture}-section-button-active`;
+        prefectureButton.className = 'sidenav-button-active';
+        prefectureTracks.style.display = 'block';
+    } else {
+        prefectureButtonActive.id = `${prefecture}-section-button`;
+        prefectureButtonActive.className = 'sidenav-button';
+        prefectureTracks.style.display = 'none';
+    }
+}
+
+function toggleProvince(province) {
+    document.getElementById(`${province}-section`).style.display = 'block';
+    document.getElementById(`${province}-section-button`).className = 'sidenav-button-active';
+}
+
+function togglePrefecture(province) {
+
+    removeSections()
+
+    if (province === 'akina' || province === 'akagi' || province === 'myogi' || province === 'usui') {
+        document.getElementById('gunma-section').style.display = 'block';
+    } else if (province === 'momiji' || province === 'irohazaka' || province === 'enna-skyline' || province === 'happogahara') {
+        document.getElementById('tochigi-section').style.display = 'block';
+    } else if (province === 'shomaru' || province === 'sadamine' || province === 'maze' || province === 'tsuchisaka') {
+        document.getElementById('saitama-section').style.display = 'block';
+    } else if (province === 'tsukuba') {
+        document.getElementById('ibaraki-section').style.display = 'block';
+    } else if (province === 'yabitsu' || province === 'nagao' || province === 'nanamagari' || province === 'tsubaki') {
+        document.getElementById('kanagawa-section').style.display = 'block';
+    } else if (province === 'chiba') {
+        document.getElementById('chiba-section').style.display = 'block';
+    } else if (province === 'tokyo') {
+        document.getElementById('tokyo-section').style.display = 'block';
+    }
+
+    toggleProvince(province)
+
+}
+
+if (document.getElementById('gunma-section-button')) {
+    document.getElementById("gunma-section-button").addEventListener('click', function() {
+        toggleTracks('gunma');
+    });
+}
+if (document.getElementById('gunma-section-button-active')) {
+    document.getElementById("gunma-section-button-active").addEventListener('click', function() {
+        toggleTracks('gunma');
+    });
+}
+
+if (document.getElementById("tochigi-section-button")) {
+    document.getElementById("tochigi-section-button").addEventListener('click', function() {
+        toggleTracks('tochigi');
+    });
+}
+if (document.getElementById("tochigi-section-button-active")) {
+    document.getElementById("tochigi-section-button-active").addEventListener('click', function() {
+        toggleTracks('tochigi');
+    });
+}
+
+if (document.getElementById("saitama-section-button")) {
+    document.getElementById("saitama-section-button").addEventListener('click', function() {
+        toggleTracks('saitama');
+    });
+}
+if (document.getElementById("saitama-section-button-active")) {
+    document.getElementById("saitama-section-button-active").addEventListener('click', function() {
+        toggleTracks('saitama');
+    });
+}
+
+if (document.getElementById("ibaraki-section-button")) {
+    document.getElementById("ibaraki-section-button").addEventListener('click', function() {
+        toggleTracks('ibaraki');
+    });
+}
+if (document.getElementById("ibaraki-section-button-active")){
+    document.getElementById("ibaraki-section-button-active").addEventListener('click', function() {
+        toggleTracks('ibaraki');
+    });
+}
+
+if (document.getElementById("kanagawa-section-button")) {
+    document.getElementById("kanagawa-section-button").addEventListener('click', function() {
+        toggleTracks('kanagawa');
+    });
+}
+if (document.getElementById("kanagawa-section-button-active")) {
+    document.getElementById("kanagawa-section-button-active").addEventListener('click', function() {
+        toggleTracks('kanagawa');
+    });
+}
+
+if (document.getElementById('akina-section-button')) {
+    document.getElementById('akina-section-button').addEventListener('click', function() {
+        togglePrefecture('akina');
+    })
+}
+
+if (document.getElementById('akagi-section-button')) {
+    document.getElementById('akagi-section-button').addEventListener('click', function() {
+        togglePrefecture('akagi');
+    })
+}
+
+if (document.getElementById('myogi-section-button')) {
+    document.getElementById('myogi-section-button').addEventListener('click', function() {
+        togglePrefecture('myogi');
+    })
+}
+
+if (document.getElementById('usui-section-button')) {
+    document.getElementById('usui-section-button').addEventListener('click', function() {
+        togglePrefecture('usui');
+    })
+}
+
+if (document.getElementById('momiji-section-button')) {
+    document.getElementById('momiji-section-button').addEventListener('click', function() {
+        togglePrefecture('momiji');
+    })
+}
+
+if (document.getElementById('irohazaka-section-button')) {
+    document.getElementById('irohazaka-section-button').addEventListener('click', function() {
+        togglePrefecture('irohazaka');
+    })
+}
+
+if (document.getElementById('enna-skyline-section-button')) {
+    document.getElementById('enna-skyline-section-button').addEventListener('click', function() {
+        togglePrefecture('enna-skyline');
+    })
+}
+
+if (document.getElementById('happogahara-section-button')) {
+    document.getElementById('happogahara-section-button').addEventListener('click', function() {
+        togglePrefecture('happogahara');
+    })
+}
+
+if (document.getElementById('shomaru-section-button')) {
+    document.getElementById('shomaru-section-button').addEventListener('click', function() {
+        togglePrefecture('shomaru');
+    })
+}
+
+if (document.getElementById('sadamine-section-button')) {
+    document.getElementById('sadamine-section-button').addEventListener('click', function() {
+        togglePrefecture('sadamine');
+    })
+}
+
+if (document.getElementById('maze-section-button')) {
+    document.getElementById('maze-section-button').addEventListener('click', function() {
+        togglePrefecture('maze');
+    })
+}
+
+if (document.getElementById('tsuchisaka-section-button')) {
+    document.getElementById('tsuchisaka-section-button').addEventListener('click', function() {
+        togglePrefecture('tsuchisaka');
+    })
+}
+
+if (document.getElementById('tsukuba-section-button')) {
+    document.getElementById('tsukuba-section-button').addEventListener('click', function() {
+        togglePrefecture('tsukuba');
+    })
+}
+
+if (document.getElementById('yabitsu-section-button')) {
+    document.getElementById('yabitsu-section-button').addEventListener('click', function() {
+        togglePrefecture('yabitsu');
+    })
+}
+
+if (document.getElementById('nagao-section-button')) {
+    document.getElementById('nagao-section-button').addEventListener('click', function() {
+        togglePrefecture('nagao');
+    })
+}
+
+if (document.getElementById('nanamagari-section-button')) {
+    document.getElementById('nanamagari-section-button').addEventListener('click', function() {
+        togglePrefecture('nanamagari');
+    })
+}
+
+if (document.getElementById('tsubaki-section-button')) {
+    document.getElementById('tsubaki-section-button').addEventListener('click', function() {
+        togglePrefecture('tsubaki');
+    })
+}
+
+$("#akina-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'akina')
+    window.location.replace('provinces.html')
+});
+$("#akagi-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'akagi')
+    window.location.replace('provinces.html')
+});
+$("#myogi-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'myogi')
+    console.log("myogi")
+    window.location.replace('provinces.html')
+});
+$("#usui-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'usui')
+    console.log("usui")
+    window.location.replace('provinces.html')
+});
+$("#momiji-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'momiji')
+    console.log("momiji")
+    window.location.replace('provinces.html')
+});
+$("#irohazaka-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'irohazaka')
+    console.log("irohazaka")
+    window.location.replace('provinces.html')
+});
+$("#enna-skyline-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'enna-skyline')
+    console.log("enna-skyline")
+    window.location.replace('provinces.html')
+});
+$("#happogahara-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'happogahara')
+    console.log("happogahara")
+    window.location.replace('provinces.html')
+});
+$("#shomaru-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'shomaru')
+    console.log("shomaru")
+    window.location.replace('provinces.html')
+});
+$("#sadamine-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'sadamine')
+    console.log("sadamine")
+    window.location.replace('provinces.html')
+});
+$("#maze-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'maze')
+    console.log("maze")
+    window.location.replace('provinces.html')
+});
+$("#tsuchisaka-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'tsuchisaka')
+    console.log("tsuchisaka")
+    window.location.replace('provinces.html')
+});
+$("#tsukuba-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'tsukuba')
+    console.log("tsukuba")
+    window.location.replace('provinces.html')
+});
+$("#yabitsu-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'yabitsu')
+    console.log("yabitsu")
+    window.location.replace('provinces.html')
+});
+$("#nagao-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'nagao')
+    console.log("nagao")
+    window.location.replace('provinces.html')
+});
+$("#nanamagari-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'nanamagari')
+    console.log("nanamagari")
+    window.location.replace('provinces.html')
+});
+$("#tsubaki-track").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('province', 'tsubaki')
+    console.log("tsubaki")
+    window.location.replace('provinces.html')
+});
+
+function toggleCar(car){
+    const cars = ['ae86', 'fd3s', 'fc3s'];
+
+    cars.forEach(function(carRemove){
+        if (document.getElementById(`${carRemove}-section`)) {
+            document.getElementById(`${carRemove}-section`).style.display = 'none';
+            document.getElementById(`${carRemove}-section-button`).className = 'sidenav-button';
+        }
+    })
+
+    if (document.getElementById(`${car}-section`)) {
+        document.getElementById(`${car}-section`).style.display = 'block';
+        document.getElementById(`${car}-section-button`).className = 'sidenav-button-active';
+    }
+}
+
+if (document.getElementById('ae86-section-button')) {
+    document.getElementById('ae86-section-button').addEventListener('click', function() {
+        toggleCar('ae86');
+    })
+}
+if (document.getElementById('fd3s-section-button')) {
+    document.getElementById('fd3s-section-button').addEventListener('click', function() {
+        toggleCar('fd3s');
+    })
+}
+if (document.getElementById('fc3s-section-button')) {
+    document.getElementById('fc3s-section-button').addEventListener('click', function() {
+        toggleCar('fc3s');
+    })
+}
+
+$("#ae86-button").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('car', 'ae86')
+    console.log("ae86")
+    window.location.replace('cars.html')
+});
+$("#fd3s-button").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('car', 'fd3s')
+    console.log("fd3s")
+    window.location.replace('cars.html')
+});
+$("#fc3s-button").on('click', function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('car', 'fc3s')
+    console.log("fc3s")
+    window.location.replace('cars.html')
 });
